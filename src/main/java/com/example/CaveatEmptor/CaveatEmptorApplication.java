11481b3be3a84806fbade563ad1437313c90ffb1
Jeonghwan Lee <brianlee6484@gmail.com>
2023-10-28 23:46:43 +0900

    feat: initial project settings

---
+++ src/main/java/com/example/CaveatEmptor/CaveatEmptorApplication.java
@@ -0,0 +1,13 @@
+package com.example.CaveatEmptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveatEmptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaveatEmptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CaveatEmptor/CaveatEmptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CaveatEmptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaveatEmptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
