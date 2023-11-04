fa7ff87fd48a063292c0b8fa7a45cb1e09044958
Jeonghwan Lee <brianlee6484@gmail.com>
2023-11-05 01:36:08 +0900

    feat: making abstract class for base test template to be extended for other tests

--- src/test/java/com/example/CaveatEmptor/CaveatEmptorApplicationTests.java
+++ src/test/java/com/example/CaveatEmptor/CaveatEmptorApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.CaveatEmptor;
 
+import com.example.CaveatEmptor.user.User;
+import com.example.CaveatEmptor.user.UserRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CaveatEmptorApplicationTests {
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
-	@Test
-	void contextLoads() {
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class CaveatEmptorApplicationTests {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeAll
+	void beforeAll(){
+		userRepository.saveAll(generateUsers());
+	}
+
+	private static List<User> generateUsers(){
+		List<User> list = new ArrayList<>();
+		User john = new User("john", LocalDate.of(2021, Month.APRIL, 13));
+		list.add(john);
+		return list;
+	}
+
+	@AfterAll
+	void afterAll(){
+		userRepository.deleteAll();
 	}
 
 }
---
+++ src/test/java/com/example/CaveatEmptor/SampleTest.java
@@ -0,0 +1,5 @@
+package com.example.CaveatEmptor;
+
+public class SampleTest extends CaveatEmptorApplicationTests{
+
+}
